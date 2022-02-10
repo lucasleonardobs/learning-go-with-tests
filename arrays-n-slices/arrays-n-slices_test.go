c4f30d4188b7d57ec6671abad95fe408bd13d246
Lucas Leonardo <lucas.leonardo@g.globo>
2022-02-09 22:33:17 -0300

    Implement slice sum all the rest function and test

--- arrays-n-slices/arrays-n-slices_test.go
+++ arrays-n-slices/arrays-n-slices_test.go
@@ -29,3 +29,36 @@ func TestSumAll(t *testing.T) {
 		t.Errorf("response '%v', expected '%v'", response, expected_response)
 	}
 }
+
+func TestSumAllTheRest(t *testing.T) {
+	verifySums := func(t *testing.T, response, expected_response []int) {
+		t.Helper()
+
+		if !reflect.DeepEqual(response, expected_response) {
+			t.Errorf("response '%v', expected '%v'", response, expected_response)
+
+		}
+	}
+
+	t.Run("when a valid slice is given, sum all the rest", func(t *testing.T) {
+		numbers1 := []int{1, 2, 3}
+		numbers2 := []int{4, 5}
+
+		response := SumAllTheRest(numbers1, numbers2)
+
+		expected_response := []int{5, 5}
+
+		verifySums(t, response, expected_response)
+	})
+
+	t.Run("when a empty slice is given, return 0 for it", func(t *testing.T) {
+		numbers1 := []int{}
+		numbers2 := []int{4, 5}
+
+		response := SumAllTheRest(numbers1, numbers2)
+
+		expected_response := []int{0, 5}
+
+		verifySums(t, response, expected_response)
+	})
+}
