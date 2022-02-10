8af297f9d6479fc2e76db62adcbe0f9be3ec687a
Lucas Leonardo <lucas.leonardo@g.globo>
2022-02-09 21:52:22 -0300

    Implement a slice sum all function and test

--- arrays-n-slices/arrays-n-slices_test.go
+++ arrays-n-slices/arrays-n-slices_test.go
@@ -1,17 +1,31 @@
 package arrays_n_slices
 
-import "testing"
+import (
+	"reflect"
+	"testing"
+)
 
 func TestSum(t *testing.T) {
-	t.Run("colection of 5 numbers", func(t *testing.T) {
-		numbers := []int{1, 2, 3, 4, 5}
+	numbers := []int{1, 2, 3, 4, 5}
 
-		response := Sum(numbers)
+	response := Sum(numbers)
 
-		expected_response := 15
+	expected_response := 15
 
-		if response != expected_response {
-			t.Errorf("response '%d', expected '%d' from '%v", response, expected_response, numbers)
-		}
-	})
+	if response != expected_response {
+		t.Errorf("response '%d', expected '%d' from '%v", response, expected_response, numbers)
+	}
+}
+
+func TestSumAll(t *testing.T) {
+	numbers1 := []int{1, 2, 3}
+	numbers2 := []int{4, 5}
+
+	response := SumAll(numbers1, numbers2)
+
+	expected_response := []int{6, 9}
+
+	if !reflect.DeepEqual(response, expected_response) {
+		t.Errorf("response '%v', expected '%v'", response, expected_response)
+	}
 }
