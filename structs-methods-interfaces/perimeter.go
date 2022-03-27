1c26b35fd54e80219e8e20c92f0dc9ee6dbecca9
Lucas Leonardo <lucas.leonardo@g.globo>
2022-03-27 15:53:40 -0300

    Implement a geometry problems and tests

---
+++ structs-methods-interfaces/perimeter.go
@@ -0,0 +1,28 @@
+package structs_methods_n_interfaces
+
+import "math"
+
+type Shape interface {
+	Area() float64
+}
+
+type Rectangle struct {
+	Lenght float64
+	Height float64
+}
+
+func (r Rectangle) Area() float64 {
+	return r.Lenght * r.Height
+}
+
+type Circle struct {
+	Radiu float64
+}
+
+func (c Circle) Area() float64 {
+	return math.Pi * c.Radiu * c.Radiu
+}
+
+func Perimeter(rectangle Rectangle) float64 {
+	return 2 * (rectangle.Lenght + rectangle.Height)
+}
